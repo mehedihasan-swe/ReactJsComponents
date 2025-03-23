2abb1377e42f987f277c314e1ef1de03a2f8bea9
mehedihasan-swe <mh121499@gmail.com>
2025-03-23 17:11:50 +0600

    [Style][Feat] Added RadioGroup

--- reactcomponents/src/components/ComButton.js
+++ reactcomponents/src/components/ComButton.js
@@ -1,4 +1,4 @@
-import { Badge, badgeClasses, Button, IconButton, InputAdornment, Stack, TextField, Tooltip } from '@mui/material'
+import { Badge, badgeClasses, Button, FormControl, FormControlLabel, FormLabel, IconButton, InputAdornment, Radio, RadioGroup, Stack, TextField, Tooltip } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete';
 import SendIcon from '@mui/icons-material/Send';
 import React, { useState } from 'react'
@@ -18,6 +18,7 @@ const CartBadge = styled(Badge)`
 
 export default function ComButton() {
   const [data, setData] = useState('');
+  const [radData, setradData] = useState('')
   return (
     <>
       <Stack direction="row" spacing={2} display="flex" justifyContent="center" alignContent="center" marginTop="30vh">
@@ -40,7 +41,7 @@ export default function ComButton() {
             <MailIcon />
           </Badge>
         </Tooltip>
-        
+
       </Stack>
 
       <Stack direction="row" spacing={2} display="flex" justifyContent="center" alignContent="center" marginTop="50px">
@@ -60,21 +61,76 @@ export default function ComButton() {
           }}
 
         />
-     
+
         <TextField
-        variant='outlined'
-        label="Please write here"
-        value={data}
-        error={!data}
-        helperText={!data ? 'requred this fild': 'done'}
-        onChange={(e)=>setData(e.target.value)}
+
+          variant='outlined'
+          label="Please write here"
+          value={data}
+          error={!data}
+          helperText={!data ? 'requred this fild' : 'done'}
+          onChange={(e) => setData(e.target.value)}
+
         />
-
-
 
         <TextField label="Filled" variant='filled' />
         <TextField label="standard" variant='standard' />
 
+      </Stack>
+
+      <Stack direction="row" display='flex' justifyContent='center' alignContent='center' marginTop="50px" >
+        <Radio
+          value="Spanish"
+          label="Spanish"
+          checked={radData === "Spanish"}
+          onChange={(e) => setradData(e.target.value)}
+        />
+        <Radio
+          value="English"
+          label="English"
+          checked={radData === "English"}
+          onChange={(e) => setradData(e.target.value)}
+        />
+
+      </Stack>
+      <Stack>
+        <p>{radData}</p>
+      </Stack>
+
+      <Stack direction='row' spacing={2} alignContent='center' marginTop="50px" justifyContent='center'>
+        <FormControl>
+          <FormLabel><p>Your language</p></FormLabel>
+          <RadioGroup 
+            name='language you speak'
+            aria-labelledby='language_you_speak'
+            value={data}
+            onChange={e=>setData(e.target.value)}
+          >
+           <FormControlLabel 
+           value='Spanish'
+           control={<Radio/>}
+           label="Spanish" 
+           />
+
+          <FormControlLabel 
+           value='German'
+           control={<Radio/>}
+           label="German" 
+          
+           />
+
+          <FormControlLabel 
+           value='English'
+           control={<Radio/>}
+           label="English"
+          
+           />
+
+          </RadioGroup>
+        </FormControl>
+      </Stack>
+      <Stack direction='row' spacing={2} alignContent='center' marginTop="50px" marginBottom="200px" justifyContent='center'>
+        <p>{data}</p>
       </Stack>
     </>
   )
